088e85c6f9589f2042c9cc25f0da2b0a6a167e0b
Moringa School <maryanneanyango98@gmail.com>
2022-09-11 22:09:22 +0300

    completed file

---
+++ my-app/src/components/BookingForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+function BookingForm(){
+    return (
+        <div className='booking'>
+
+        <form>
+
+        
+            <input type="text"name="name"placeholder="name">
+                
+            </input>
+        </form>
+        </div>
+    )
+}
+export default BookingForm
